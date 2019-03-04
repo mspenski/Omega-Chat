7ed2d7efd24ba444cae54287d818a4e255df5a63
mspenski <44811974+mspenski@users.noreply.github.com>
2019-03-04 13:19:09 -0500

    added files for some reason app broke

---
+++ server/controllers/api/messageBoardController.js
@@ -0,0 +1,9 @@
+// const messageBoardController = require('express').Router();
+
+// const db = require('../../models');
+// const { JWTVerifier } = require('../../lib/passport');
+// const jwt = require('jsonwebtoken');
+
+// messageBoardController.get('/me', JWTVerifier, (req, res) => {
+//   res.json(req.user);
+// });
