bad1462b38863b7ec29d40a353bb8cb7390c2406
Steve Shannon <you@example.com>
2019-03-05 11:05:41 -0500

    welcoming home page. still need images

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "../Home/Home.css"
 
 // import logo from './logo.svg';
 
@@ -8,19 +9,79 @@ class HomePage extends Component {
       <div className='Home'>
         <div className='row'>
           <div className='col'>
-            
+            <div id="carousel-example" className="carousel slide" data-ride="carousel">
+              {/* Indicators */}
+              <div className="carousel-wrapper">
+              <br></br>
+            <div className='navbar-brand logo' to='#'>Welcome to Ωmega Chat</div>
             <br></br>
-     <div className='navbar-brand logo' to='#'>Welcome to Ωmega Chat</div>
-     <br></br>
-     <div className='navbar-brand logo' to='#'>Our Fraternity Networking App</div>
-     <br></br>
-     <br></br>
-     <div className='navbar-brand logo' to='#'>Connected Brothership</div>
+            <div className='navbar-brand logo' to='#'>Our Fraternity networking app</div>
+            <br></br>
+            <br></br>
+            <div className='navbar-brand logo' to='#'>Connected Brothership</div>
+                
+              </div>
+              {/* Wrapper for slides */}
+              <div className="carousel-inner" role="listbox">
+                <div className="item active">
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <div className="img-wrapper">
+                        <img src="http://kids.nationalgeographic.com/content/dam/kids/photos/articles/Nature/Q-Z/siberian-tiger-profile.jpg" />
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                      <div className="carousel-caption">
+                        <h2>Roster</h2>
+                        <p>See who is part of your greek life.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="item">
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <div className="img-wrapper">
+                        <img src="http://www.govisitcostarica.com/images/photos/full-hot-air-balloons-near-arenal.jpg" />
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                      <div className="carousel-caption">
+                        <h2>Calendar</h2>
+                        <p>Be able to schedule yourself and plan ahead as a group</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="item">
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <div className="img-wrapper">
+                        <img src="http://www.earthrandom.com/wp-content/uploads/2013/01/Virginia1.jpg" />
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                      <div className="carousel-caption">
+                        <h2>Message Board</h2>
+                        <p>The online group chat</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
-      </div>
-    );
-  }
-}
+        </div>
 
-export default HomePage;
+
+
+
+
+
+        );
+      }
+    }
+    
+    export default HomePage;
