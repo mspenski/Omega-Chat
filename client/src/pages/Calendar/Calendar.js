d5b842a687f3ac239bba9d7b6aa12c187808b105
mspenski <44811974+mspenski@users.noreply.github.com>
2019-03-06 00:28:05 -0500

    minor improvements

--- client/src/pages/Calendar/Calendar.js
+++ client/src/pages/Calendar/Calendar.js
@@ -32,48 +32,20 @@ class Calendar extends Component {
       .then(res => this.setState({ posts: res.data }))
   }
 
-  render() {
-    const { title, start, end } = this.state
-    return (
-      <div className="posts">
-        <form className="card beer-form">
-          <div className="card-body">
-            <div className="form-group">
-              {this.state.posts.map(newPost => (
-                <p key={newPost.id}>
-                  <h4 key={newPost.id} id='new-post-title'>{newPost.title} Posted: {newPost.date}</h4>
-                  {newPost.text}<br />
-                  {/* <button onClick={() => this.handleReply(newPost.id)} id="reply-button" className="btn btn-primary reply mb-2">Reply</button>
-                <input
-                  type="text"
-                  id="post-reply"
-                  name="Reply"
-                  value={reply}
-                  onChange={this.handleInputChange}
-                  className="form-control"
-                  placeholder="Type response here"></input> */}
-                  {/* <button type="button" id="show-button" className="btn btn-primary show mb-2">Show Thread</button>
-                <button type="button" id="like-button" className="btn btn-success like mb-2"><i className="far fa-thumbs-up"></i> Like</button>
-                <button type="button" id="dislike-button" className="btn btn-danger dislike mb-2"><i className="far fa-thumbs-down"></i> Dislike</button> */}
-                </p>
-              ))}
-            </div>
-          </div>
-        </form>
-      </div>
-      // const Calendar = props => (
-      //   <div style={styles.Calendar} id="calendarDiv">
-      //     <BigCalendar
-      //       localizer={localizer}
-      //       events={[]}
-      //       startAccessor="start"
-      //       endAccessor="end"
-      //     />
-      //   </div>
 
-    )
-  }
+  // const Calendar = props => (
+  //   <div style={styles.Calendar} id="calendarDiv">
+  //     <BigCalendar
+  //       localizer={localizer}
+  //       events={[]}
+  //       startAccessor="start"
+  //       endAccessor="end"
+  //     />
+  //   </div>
+
+
 }
+
 
 export default Calendar;
 
